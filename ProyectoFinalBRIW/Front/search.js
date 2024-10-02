ed2c46dec31772478babd9192262a06fd5196694
Andrés Mena Salazar <andres_mena@live.com>
2024-10-02 17:14:09 -0600

    Feature-Paginacion
    Se realizó la paginación de búsquedas, listo para las pruebas

--- ProyectoFinalBRIW/Front/search.js
+++ ProyectoFinalBRIW/Front/search.js
@@ -1,22 +1,18 @@
-const intersperse = (arr, sep) => arr.reduce((a,v)=>[...a,v,sep],[]).slice(0,-1)
+const intersperse = (arr, sep) => arr.reduce((a, v) => [...a, v, sep], []).slice(0, -1);
 
 $(document).ready(function () {
   const suggestionsContainer = $("#suggestionsContainer");
 
   const hideDropdownIfFocusedOutside = (event) => {
     const focusedElement = event.relatedTarget;
-    if (focusedElement == null || !(focusedElement.classList.contains('dropdown-item') || focusedElement.id === 'queryInput')){
+    if (focusedElement == null || !(focusedElement.classList.contains('dropdown-item') || focusedElement.id === 'queryInput')) {
       suggestionsContainer.addClass('hidden');
     }
-  }
+  };
 
-  // Función para obtener sugerencias de ortografía desde la API de Google en español
   function mostrarSugerencias(sugerencias) {
     if (sugerencias?.length > 0) {
-      suggestionsContainer
-        .removeClass('hidden')
-        .empty();
-
+      suggestionsContainer.removeClass('hidden').empty();
       sugerencias.forEach(sugerencia => {
         const sugerenciaElemento = $("<button>")
           .addClass('dropdown-item')
@@ -29,7 +25,6 @@ $(document).ready(function () {
             suggestionsContainer.addClass('hidden').empty();
             $("#correctionContainer").empty();
           });
-
         suggestionsContainer.append(sugerenciaElemento);
       });
     } else {
@@ -37,7 +32,6 @@ $(document).ready(function () {
     }
   }
 
-  // Escuchar el evento de cambio en el input
   $("#queryInput")
     .on('input', function () {
       const palabraConsulta = $(this).val();
@@ -46,31 +40,23 @@ $(document).ready(function () {
     .on('focus', () => suggestionsContainer.removeClass('hidden'))
     .on('focusout', hideDropdownIfFocusedOutside);
 
-  // Función para obtener sugerencias de ortografía desde la API de Google en español
   function obtenerSugerencias(palabra) {
-    // Llamada a la API de Google para español
     fetch(`https://inputtools.google.com/request?text=${palabra}&itc=es-t-i0-und&num=5`)
       .then(response => response.json())
       .then(data => {
         const sugerencias = data?.[1]?.[0]?.[1] || [];
         mostrarSugerencias(sugerencias);
-
-        // Verificar si hay correcciones y mostrarlas
         if (sugerencias && sugerencias.length > 0) {
-          const correccion = sugerencias[0]; // Tomar la primera sugerencia como corrección
-
-          // Mostrar la corrección en el contenedor de correcciones
+          const correccion = sugerencias[0];
           mostrarCorreccion(correccion);
         }
       })
       .catch(error => console.error('Error al obtener sugerencias:', error));
   }
 
-  // Función para mostrar las correcciones
   function mostrarCorreccion(correccion) {
     const correctionContainer = $("#correctionContainer");
-    correctionContainer.empty(); // Limpiar el contenedor antes de mostrar la corrección
-
+    correctionContainer.empty();
     if (correccion && correccion !== $("#queryInput").val()) {
       correctionContainer
         .append('Quizás quisiste decir:  ')
@@ -86,32 +72,24 @@ $(document).ready(function () {
             })
         );
     } else {
-      correctionContainer.empty(); // Vaciar el contenedor si no hay corrección o es igual a la palabra original
+      correctionContainer.empty();
     }
   }
 
-  // Función para obtener y mostrar palabras relacionadas semánticamente
   function obtenerPalabrasRelacionadas(consulta) {
     const relatedWordsContainer = $("#relatedWords");
-
-    // Verificar que haya una consulta escrita
     if (consulta === '') {
       return;
     }
-
     fetch(`https://api.datamuse.com/words?ml=${consulta}&max=5`)
       .then(respuesta => respuesta.json())
       .then(palabras => {
-        // Limpiar el contenedor antes de mostrar nuevas palabras
         relatedWordsContainer.empty();
-
-        if(palabras.length === 0) {
+        if (palabras.length === 0) {
           return;
         }
-
         relatedWordsContainer.append('Palabras relacionadas: ');
-
-        const relatedWordsItems = palabras.map(({word}) => 
+        const relatedWordsItems = palabras.map(({ word }) =>
           $('<button>')
             .addClass('text')
             .text(word)
@@ -122,109 +100,109 @@ $(document).ready(function () {
               $("#correctionContainer").empty();
             })
         );
-
-        // Enlistar las palabras relacionadas, separadas por coma
-        intersperse(relatedWordsItems, ', ')
-          .forEach(element => {
-            relatedWordsContainer.append(element);
-          });
+        intersperse(relatedWordsItems, ', ').forEach(element => {
+          relatedWordsContainer.append(element);
+        });
       })
       .catch(error => console.error('Error al obtener palabras relacionadas:', error));
   }
 
-  function obtenerResultados(consulta) {
-    const errorMessageContainer = $('#errorMessage');
-    if (consulta === '') {
-      errorMessageContainer.removeClass('hidden');
-      $("#suggestionsContainer").empty();
-      $("#relatedWords").empty();
-      $("#searchResults").empty();
-      return;
-    }
-
-    errorMessageContainer.addClass('hidden');
-
-    // Realizar la solicitud AJAX a search.php para realizar la búsqueda en Solr
-    $.ajax({
-      url: "../Back/search.php", // Ruta a search.php desde index.html
-      data: {
-        q: consulta // La consulta que se enviará a search.php
-      },
-      success: function (respuesta) {
-        console.log("Resultados de Solr desde PHP:", respuesta);
-        // Mostrar los resultados en la sección searchResults
-        mostrarResultados(consulta, respuesta);
-      },
-      error: function (xhr, estado, error) {
-        console.error("Error al buscar en Solr desde PHP:", error);
-        $("#searchResults").empty().append("<p>Error al buscar en Solr desde PHP</p>");
-      }
-    });
-  }
-
+  // Click en el botón de ejecutar consulta
   $("#executeButton").click(function () {
-    const consulta = $("#queryInput").val(); // Obtener el valor del input
+    const consulta = $("#queryInput").val();
     obtenerResultados(consulta);
     obtenerPalabrasRelacionadas(consulta);
   });
+
+  // Manejador de eventos para enlaces de paginación
+  $(document).on('click', '.pagination-link', function (e) {
+    e.preventDefault();
+    const page = $(this).data('page');
+    const query = $("#queryInput").val();
+    obtenerResultados(query, page);
+  });
 });
 
-// Función para mostrar los resultados en la lista de búsqueda
-function mostrarResultados(consulta, respuesta) {
-  const resultsContainer = document.getElementById("searchResults");
 
-  if (respuesta.documents.length === 0) {
-    resultsContainer.replaceChildren(
-      $('<h2>')
-        .append('No se encontraron resultados para ')
-        .append(
-          $('<span>').addClass('bold').text(consulta)
-        )
-        .append(' :(')
-        [0]
-    );
+function obtenerResultados(consulta, pagina = 1) {
+  const errorMessageContainer = $('#errorMessage');
+  const queryInput = $("#queryInput").val().trim();
+
+  // Si no hay nada escrito
+  if (queryInput === '') {
+    errorMessageContainer.text('Por favor, ingresa una consulta de búsqueda.').removeClass('hidden');
+    $("#suggestionsContainer").empty();
+    $("#relatedWords").empty();
+    $("#searchResults").empty();
+    $("#paginationContainer").empty(); // Paginador en 0
     return;
+  } else {
+    errorMessageContainer.addClass('hidden'); 
   }
 
-  resultsContainer.replaceChildren(
-    ...respuesta.documents.map(({title, url, snippet}) => (
-      $('<div>')
-        .addClass('search-result')
-        .append(
-          $('<a>')
-            .addClass('result-title')
-            .attr("target", "_blank")
-            .attr("href", url)
-            .text(title)
-        ).append(
-          $('<p>')
-            .addClass('result-snippet')
-            .text(snippet)
-        )[0]
-    ))
-  )
+  // Realizar la solicitud AJAX a search.php con la paginación
+  $.ajax({
+    url: "../Back/search.php",
+    data: {
+      q: consulta,
+      pagina: pagina
+    },
+    success: function (respuesta) {
+      console.log("Llamando a mostrarPaginacion con", consulta, respuesta.paginaActual, respuesta.totalPaginas);
+      mostrarResultados(consulta, respuesta);
+      mostrarPaginacion(consulta, respuesta.paginaActual, respuesta.totalPaginas);
+    },
+    error: function (xhr, estado, error) {
+      console.error("Error al buscar en Solr desde PHP:", error);
+      $("#searchResults").empty().append("<p>Error al buscar en Solr desde PHP</p>");
+    }
+  });
 }
 
-// Función para mostrar los resultados en la lista de búsqueda
+
+function mostrarPaginacion(consulta, paginaActual, totalPaginas) {
+  const paginacionContainer = $("#paginationContainer");
+  paginacionContainer.empty();
+
+  console.log(`Generando paginación para la página ${paginaActual} de ${totalPaginas}`);
+
+  const maxPagesToShow = 10;
+  let startPage = Math.max(1, paginaActual - Math.floor(maxPagesToShow / 2));
+  let endPage = Math.min(totalPaginas, startPage + maxPagesToShow - 1);
+
+  if (paginaActual > 1) {
+    paginacionContainer.append(`<a href="#" class="pagination-link" data-page="${paginaActual - 1}">Anterior</a> `);
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    if (i === paginaActual) {
+      paginacionContainer.append(`<strong>${i}</strong> `);
+    } else {
+      paginacionContainer.append(`<a href="#" class="pagination-link" data-page="${i}">${i}</a> `);
+    }
+  }
+
+  if (paginaActual < totalPaginas) {
+    paginacionContainer.append(`<a href="#" class="pagination-link" data-page="${paginaActual + 1}">Siguiente</a>`);
+  }
+
+  console.log("HTML generado para paginación:", paginacionContainer.html());
+}
+
 function mostrarResultados(consulta, respuesta) {
   const resultsContainer = document.getElementById("searchResults");
-  const facetResultsContainer = document.getElementById("facetResults"); // Contenedor para los resultados facetados
+  const facetResultsContainer = document.getElementById("facetResults");
 
   if (respuesta.documents.length === 0) {
-    // Mostrar mensaje si no hay resultados
     resultsContainer.replaceChildren(
       $('<h2>')
         .append('No se encontraron resultados para ')
-        .append(
-          $('<span>').addClass('bold').text(consulta)
-        )
-        .append(' :(')
-        [0]
+        .append($('<span>').addClass('bold').text(consulta))
+        .append(' :(')[0]
     );
     return;
   }
 
-  // Mostrar los resultados principales de la búsqueda normal
   resultsContainer.replaceChildren(
     ...respuesta.documents.map(({ title, url, snippet }) => (
       $('<div>')
@@ -243,15 +221,12 @@ function mostrarResultados(consulta, respuesta) {
     ))
   );
 
-  // Mostrar los resultados de la búsqueda facetada si están presentes
   if (respuesta.facetResults && respuesta.facetResults.length > 0) {
-    facetResultsContainer.innerHTML = ''; // Limpiar los resultados previos
-
-    // Agregar los resultados facetados al contenedor
+    facetResultsContainer.innerHTML = '';
     respuesta.facetResults.forEach(result => {
       const facetElement = document.createElement('div');
       facetElement.classList.add('facet-result');
-      facetElement.textContent = result; // Puedes ajustar cómo muestras los resultados facetados según la estructura de datos
+      facetElement.textContent = result;
       facetResultsContainer.appendChild(facetElement);
     });
   } else {
